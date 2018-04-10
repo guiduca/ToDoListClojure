1a8531c675d4543c4b5e80f7bd8a82cc3931bc98
Johan <jpaasche@palo-it.com>
2018-04-10 14:04:19 +0800

    added example of clojure syntax

--- todo-list/src/todo_list/complex-task.cljs
+++ todo-list/src/todo_list/complex-task.cljs
@@ -1,13 +1,22 @@
 (ns todo-list.complex-task
   (:require
+    [todo-list.utils :as utils]
     [sablono.core :include-macros true :refer [html]]))
 
 (def yolo (concat 20 "px"))
 
+(defn somefunction [state yolo]
+  (print "_________HERE__________")
+  ;;print (value of key ":details"
+  ;; ( in the first element (0) in the value of keys "tasks" inside "state")
+  (print (:details (get (:tasks state) 0))))
+
+
 (defn ui-complextask "This is a complex task" [store complex-task]
   (html [:li {:key (:task-id complex-task)}
          [:div {:class-name "ctask_tile"}
-          [:h1 {:class "ctask_title"} (:title complex-task)]
+          [:h1 {:class "ctask_title"} (:title complex-task)
+           [:button {:on-click #(utils/dispatch! store somefunction "yolo")} "X"]]
           [:div {:class "task_progress_bar_container"}
            [:div {:class "task_progress_bar_content" :style {:height "20px"
                                                              :width (+(str (:progress complex-task) "%"))}}]]
