4b4bfa7417efc821a7cb69b6b12c0e30ed716a6d
Johan <jpaasche@palo-it.com>
2018-04-10 14:23:10 +0800

    adding syntax example

--- todo-list/src/todo_list/complex-task.cljs
+++ todo-list/src/todo_list/complex-task.cljs
@@ -11,15 +11,18 @@
   ;; ( in the first element (0) in the value of keys "tasks" inside "state")
   (print (:details (get (:tasks state) 0))))
 
+(defn action-remove-ctask [state complex-task]
+  "Removing a task by comparison of the whole lists that defines them"
+  (update-in state [:tasks] #(remove (fn [value] (= complex-task value)) %)))
 
 (defn ui-complextask "This is a complex task" [store complex-task]
   (html [:li {:key (:task-id complex-task)}
          [:div {:class-name "ctask_tile"}
           [:h1 {:class "ctask_title"} (:title complex-task)
-           [:button {:on-click #(utils/dispatch! store somefunction "yolo")} "X"]]
+           [:button {:on-click #(utils/dispatch! store action-remove-ctask complex-task)} "X"]]
           [:div {:class "task_progress_bar_container"}
            [:div {:class "task_progress_bar_content" :style {:height "20px"
-                                                             :width (+(str (:progress complex-task) "%"))}}]]
-          [:span "Progress : " (:progress complex-task) "%"]
+                                                             :width (+(str (:progress complex-task) "%"))}}]
+           "Progress : " (:progress complex-task) "%"]
           [:span "validated :" (:validated complex-task)]
-          [:div (:details complex-task)]]]))
+          [:div {:class "ctask_details"}(:details complex-task)]]]))
