bfa92616169348e7b3a60ebe65cffa13e07e62c6
Johan <jpaasche@palo-it.com>
2018-04-10 18:50:28 +0800

    Added first edit with css link (poor)

--- todo-list/src/todo_list/complex-task.cljs
+++ todo-list/src/todo_list/complex-task.cljs
@@ -19,21 +19,27 @@
   (assoc state :editing true))
 
 (defn ui-complextask "This is a complex task" [store complex-task]
-  (html [:li {:key (:task-id complex-task)}
-         [:div {:class-name "ctask_main"}
-          [:div {:class-name "ctask_tile"}
-           [:div {:class "ctask_title"}
-            [:span (:title complex-task)]
-            [:div {:class "task_progress_bar_container"}
-             [:div {:class (let [n (:progress complex-task)]
-                             (cond  (<= n 33) "task_progress_bar_content_short"
-                                    (<= n 66) "task_progress_bar_content_medium"
-                                    (> n 66) "task_progress_bar_content_end"))
-                    :style {:width (+(str (:progress complex-task) "%"))}}]
-             [:p "Progress : " (:progress complex-task) "%"]]
-            [:div {:class "ctask_title_buttondiv"}
-              [:button {:on-click #(utils/dispatch! store action-trigger-edit-ctask)} "Edit"]
-              [:button {:on-click #(utils/dispatch! store action-remove-ctask complex-task)} "X"]]]]
-          [:div {:class "ctask_details"}
-           [:span "validated :" (:validated complex-task)]
-           [:div (:details complex-task)]]]]))
+  (let [htmlclass {:keyname :span}]
+    (html [:li {:key (:task-id complex-task)}
+           [:div {:class-name "ctask_main"}
+            [:div {:class-name "ctask_tile"}
+               ; [:input {:default-value (:title complex-task)}])
+
+             [:div {:class "ctask_title"}
+              ; (print (:keyname htmlclass))
+              (if (get @store :editing)
+                [:input {:value "example"}]
+                [:span (:title complex-task)])
+              [:div {:class "task_progress_bar_container"}
+               [:div {:class (let [n (:progress complex-task)]
+                               (cond  (<= n 33) "task_progress_bar_content_short"
+                                 (<= n 66) "task_progress_bar_content_medium"
+                                 (> n 66) "task_progress_bar_content_end"))
+                      :style {:width (+(str (:progress complex-task) "%"))}}]
+               [:p "Progress : " (:progress complex-task) "%"]]
+              [:div {:class "ctask_title_buttondiv"}
+               [:button {:on-click #(utils/dispatch! store action-trigger-edit-ctask)} "Edit"]
+               [:button {:on-click #(utils/dispatch! store action-remove-ctask complex-task)} "X"]]]]
+            [:div {:class "ctask_details"}
+             [:span "validated :" (:validated complex-task)]
+             [:div (:details complex-task)]]]])))
